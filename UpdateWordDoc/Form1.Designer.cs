95790cd842e10065956348b34125b15d5f18757a
arman <arman@WINDOWS-E1OTB06>
2023-01-10 08:42:02 -0700

    Add project files.

---
+++ UpdateWordDoc/Form1.Designer.cs
@@ -0,0 +1,223 @@
+﻿
+namespace UpdateWordDoc
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.positionBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.nameBox = new System.Windows.Forms.TextBox();
+            this.companyNameBox = new System.Windows.Forms.TextBox();
+            this.addressBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.companyActivityBox = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.skillsBox = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.button1.Location = new System.Drawing.Point(362, 371);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(192, 67);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Generate Resume";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label1.Location = new System.Drawing.Point(84, 111);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(117, 20);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Position name: ";
+            // 
+            // positionBox
+            // 
+            this.positionBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.positionBox.Location = new System.Drawing.Point(235, 105);
+            this.positionBox.Name = "positionBox";
+            this.positionBox.Size = new System.Drawing.Size(486, 26);
+            this.positionBox.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label2.Location = new System.Drawing.Point(84, 143);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(98, 20);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Hiring name:";
+            // 
+            // nameBox
+            // 
+            this.nameBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.nameBox.Location = new System.Drawing.Point(235, 137);
+            this.nameBox.Name = "nameBox";
+            this.nameBox.Size = new System.Drawing.Size(486, 26);
+            this.nameBox.TabIndex = 6;
+            // 
+            // companyNameBox
+            // 
+            this.companyNameBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.companyNameBox.Location = new System.Drawing.Point(235, 169);
+            this.companyNameBox.Name = "companyNameBox";
+            this.companyNameBox.Size = new System.Drawing.Size(486, 26);
+            this.companyNameBox.TabIndex = 7;
+            // 
+            // addressBox
+            // 
+            this.addressBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.addressBox.Location = new System.Drawing.Point(235, 204);
+            this.addressBox.Multiline = true;
+            this.addressBox.Name = "addressBox";
+            this.addressBox.Size = new System.Drawing.Size(486, 75);
+            this.addressBox.TabIndex = 8;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label3.Location = new System.Drawing.Point(84, 175);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(124, 20);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Company name:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label4.Location = new System.Drawing.Point(84, 210);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(72, 20);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Address:";
+            // 
+            // companyActivityBox
+            // 
+            this.companyActivityBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.companyActivityBox.Location = new System.Drawing.Point(235, 285);
+            this.companyActivityBox.Name = "companyActivityBox";
+            this.companyActivityBox.Size = new System.Drawing.Size(486, 26);
+            this.companyActivityBox.TabIndex = 11;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label5.Location = new System.Drawing.Point(84, 291);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(131, 20);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Company activity:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label6.Location = new System.Drawing.Point(384, 54);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(148, 20);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "Customize Resume";
+            // 
+            // skillsBox
+            // 
+            this.skillsBox.AutoSize = true;
+            this.skillsBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.skillsBox.Location = new System.Drawing.Point(84, 323);
+            this.skillsBox.Name = "skillsBox";
+            this.skillsBox.Size = new System.Drawing.Size(49, 20);
+            this.skillsBox.TabIndex = 14;
+            this.skillsBox.Text = "Skills:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.textBox1.Location = new System.Drawing.Point(235, 317);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(486, 26);
+            this.textBox1.TabIndex = 15;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.skillsBox);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.companyActivityBox);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.addressBox);
+            this.Controls.Add(this.companyNameBox);
+            this.Controls.Add(this.nameBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.positionBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox positionBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox nameBox;
+        private System.Windows.Forms.TextBox companyNameBox;
+        private System.Windows.Forms.TextBox addressBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox companyActivityBox;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label skillsBox;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ UpdateWordDoc/Form1.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Windows.Forms;
+using Microsoft.Office.Interop.Word;
+
+namespace UpdateWordDoc
+{
+    public partial class Form1 : Form
+    {
+        public object Application { get; private set; }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void findAndReplace(Microsoft.Office.Interop.Word.Application app, Document doc, string findText, string replaceText)
+        {
+            //create the findObject
+            Find findObject = app.Selection.Find;
+            
+            //ClearFormatting needs to be called on findObject && findObject.Replacement
+            findObject.ClearFormatting();
+            findObject.Text = findText;
+            findObject.Replacement.ClearFormatting();
+            findObject.Replacement.Text = replaceText;
+
+            //define missingVal (object representation of null value)
+            object missingVal = System.Reflection.Missing.Value;
+
+            //defind replaceVal -> replaceAll
+            object replaceVal = WdReplace.wdReplaceAll;
+            findObject.Execute(ref missingVal, ref missingVal, ref missingVal, ref missingVal, ref missingVal, ref missingVal, ref missingVal, ref missingVal, ref missingVal, ref missingVal, ref replaceVal, ref missingVal, ref missingVal, ref missingVal, ref missingVal);
+            
+            //iterate all shapes in doc (SHAPES INCLUDE text objects, drawings, shapes, pictures, OLE objects, ActiveX controls, and callouts. EXCLUDES headers & footers) 
+            Shapes shapes = doc.Shapes;
+            foreach (Shape shape in shapes)
+            {
+                //if shape has text, find and replace
+                if (shape.TextFrame.HasText != 0)
+                {
+                    string initialText = shape.TextFrame.TextRange.Text;
+                    string resultText = initialText.Replace(findText, replaceText);
+                    if (initialText != resultText)
+                    {
+                        shape.TextFrame.TextRange.Text = resultText;
+                    }
+                }
+            }
+        }
+
+        private string getDocPath() {
+            OpenFileDialog fileDialog = new OpenFileDialog();
+            fileDialog.ShowDialog();
+            return fileDialog.FileName.ToString(); 
+        }
+
+        private string copyAndRenameDoc(string position, string companyName) {
+            //select the doc to open
+            string docPath = getDocPath();
+
+            //split off the file extension 
+            string[] splitPathArr = docPath.Split('.');
+
+            //build the new doc path
+            string newDocPath = splitPathArr[0] + " - " + position + " - " + companyName + ".docx";
+
+            //copy and rename the doc
+            System.IO.File.Copy(docPath, newDocPath);
+
+            //return the new path
+            return newDocPath;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //these are defined in the word doc for quick find and replace operations
+            const string positionText = "{POSITION_NAME}";
+            const string hiringNameText = "{HIRING_NAME}";
+            const string companyNameText = "{COMPANY_NAME}";
+            const string addressText = "{ADDRESS}";
+            const string companyActivityText = "{COMPANY_ACTIVITY}";
+            const string skillsText = "{SKILL_1 • SKILL_2 • SKILL_3}";
+
+            //get data from text boxes
+            string positionData = positionBox.Text;
+            string hiringNameData = nameBox.Text;
+            string companyNameData = companyNameBox.Text;
+            string addressData = addressBox.Text;
+            string companyActivityData = companyActivityBox.Text;
+            string skillsData = skillsBox.Text;
+
+            //copy and rename the doc
+            string newDocPath = copyAndRenameDoc(positionData, companyNameData);
+
+            //create Word application object
+            Microsoft.Office.Interop.Word.Application app = new Microsoft.Office.Interop.Word.Application();
+            Document doc = app.Documents.Open(newDocPath);
+
+            //find and replace the text in the new Word doc
+            findAndReplace(app, doc, positionText, positionData);
+            findAndReplace(app, doc, hiringNameText, hiringNameData);
+            findAndReplace(app, doc, companyNameText, companyNameData);
+            findAndReplace(app, doc, addressText, addressData);
+            findAndReplace(app, doc, companyActivityText, companyActivityData);
+            findAndReplace(app, doc, skillsText, skillsData);
+
+            //save new doc, kill the process
+            doc.Save();
+            app.Quit();
+        }
+    }
+}
